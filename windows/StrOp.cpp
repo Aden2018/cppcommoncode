bd22efe41a258b4eee44cd5c9f54d7d21c258d53
a-wander <zhy905692718@163.com>
2016-12-06 23:46:31 +0800

    modify mbs unicode convert

--- windows/StrOp.cpp
+++ windows/StrOp.cpp
@@ -1,7 +1,7 @@
 #include "StrOp.h"
 #include <Windows.h>
 
-void CStrOp::TcharToChar (const TCHAR * tchar, char * _char)
+void CStrOp::TcharToChar (const wchar_t * tchar, char * _char)
 {
 	//获取字节长度
 	int iLength = WideCharToMultiByte(CP_ACP, 0, tchar, -1, NULL, 0, NULL, NULL);
@@ -9,7 +9,7 @@ void CStrOp::TcharToChar (const TCHAR * tchar, char * _char)
 	WideCharToMultiByte(CP_ACP, 0, tchar, -1, _char, iLength, NULL, NULL); 
 }
 
-void CStrOp::CharToTchar (const char * _char, TCHAR * tchar)
+void CStrOp::CharToTchar (const char * _char, wchar_t * tchar)
 {
 	int iLength = MultiByteToWideChar (CP_ACP, 0, _char, strlen (_char) + 1, NULL, 0);
 	MultiByteToWideChar (CP_ACP, 0, _char, strlen(_char) + 1, tchar, iLength);
--- windows/StrOp.h
+++ windows/StrOp.h
@@ -7,9 +7,9 @@ class CStrOp
 public:
 	//将TCHAR转为char
 	//*tchar是TCHAR类型指针，*_char是char类型指针
-	static void TcharToChar (const TCHAR * tchar, char * _char);
+	static void TcharToChar (const wchar_t * tchar, char * _char);
 	//将char转为TCHAR
 	//*tchar是TCHAR类型指针，*_char是char类型指针
-	static void CharToTchar (const char * _char, TCHAR * tchar);
+	static void CharToTchar (const char * _char, wchar_t * tchar);
 };
 #endif //STROP_H_
